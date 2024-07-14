4ead88c278094bc9a67fc26d838de80db1c77560
Supphitan Paksawad <135766352+OSP101@users.noreply.github.com>
2024-07-14 10:34:19 +0700

    Update shorten.ts

--- server/api/shorten.ts
+++ server/api/shorten.ts
@@ -3,13 +3,13 @@ import mysql from 'mysql2/promise'
 import { nanoid } from 'nanoid'
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "shorturl",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
 export default defineEventHandler(async (event) => {
